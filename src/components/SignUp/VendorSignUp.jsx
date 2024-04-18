8b3cd17529140cfa14b25cd30ac19f5ad98d5761
lawsonjerry <williambookman@pursuit.org>
2024-04-18 17:56:39 -0400

    refactored vendor sign up component

--- src/components/SignUp/VendorSignUp.jsx
+++ src/components/SignUp/VendorSignUp.jsx
@@ -2,39 +2,41 @@ import React, {useState} from 'react'
 import { useNavigate} from 'react-router-dom';
 import {create} from  "../../Helper/VendorHelper"
 
+
+
 const VendorSignUp = () => {
     const [shopName, setShopName] = useState('');
-    const [location, setLocation] = useState('');
-    const [marketProducts, setMarketProducts] = useState('');
-    const [hours, setHours] = useState('');
-    const [paymentTypes, setPaymentTypes] = useState('');
+    // const [localeId, setLocaleId] = useState(1);
+    const localeId = 1;
+    const userId = 3;
+    // const [location, setLocation] = useState('');
+    // const [marketProducts, setMarketProducts] = useState('');
+    // const [hours, setHours] = useState('');
+    // const [paymentTypes, setPaymentTypes] = useState('');
     const [errorMessage, setErrorMessage] = useState(null);
+    // const [formData, setFormData] = useState({
+
+    // })
     const navigate = useNavigate();
   
     const handleSubmit = async (event) => {
       event.preventDefault();
   
       try {
-        const response = await fetch(create, { 
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
+         await create({ 
+         
             shopName,
-            location,
-            marketProducts,
-            hours,
-            paymentTypes,
-          }),
-        });
-  
-        if (!response.ok) {
-          throw new Error(`Error creating vendor: ${response.statusText}`);
-        }
-  
-        const vendorData = await response.json();
-        console.log('Vendor created successfully:', vendorData);
+            // location,
+            // marketProducts,
+            // hours,
+            // paymentTypes,
+            userId,
+            localeId
+        })
+        console.log('Vendor created successfully:')
+ 
         alert('Vendor created successfully!')
-        navigate('/vendorDash'); // Redirect to vendor dashboard after signup
+        // navigate('/vendorDash'); // Redirect to vendor dashboard after signup
       } catch (error) {
         console.error('Error creating vendor:', error);
         setErrorMessage(error.message);
@@ -48,14 +50,14 @@ const VendorSignUp = () => {
         <form onSubmit={handleSubmit}>
           <label htmlFor="shopName">Shop Name:</label>
           <input type="text" id="shopName" name="shopName" value={shopName} onChange={(e) => setShopName(e.target.value)} required />
-          <label htmlFor="location">Location:</label>
+          {/* <label htmlFor="location">Location:</label>
           <input type="text" id="location" name="location" value={location} onChange={(e) => setLocation(e.target.value)} required />
           <label htmlFor="marketProducts">Market Products:</label>
           <textarea id="marketProducts" name="marketProducts" value={marketProducts} onChange={(e) => setMarketProducts(e.target.value)} required />
           <label htmlFor="hours">Hours of Operation:</label>
           <input type="text" id="hours" name="hours" value={hours} onChange={(e) => setHours(e.target.value)} required />
           <label htmlFor="paymentTypes">Payment Types Accepted:</label>
-          <input type="text" id="paymentTypes" name="paymentTypes" value={paymentTypes} onChange={(e) => setPaymentTypes(e.target.value)} required />
+          <input type="text" id="paymentTypes" name="paymentTypes" value={paymentTypes} onChange={(e) => setPaymentTypes(e.target.value)} required /> */}
           <button type="submit">Sign Up as Vendor</button>
         </form>
       </div>
