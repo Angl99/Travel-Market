3533d396100e39a0d713c238ecb72cd4f28ea1bc
lawsonjerry <williambookman@pursuit.org>
2024-04-17 21:15:23 -0400

    created a basic  vendor dashboard which contains a link to create a new product, and manage products

---
+++ src/components/Vendor-view/Vendor-dashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const VendorDashboard = () => {
+  return (
+    <div className="vendor-dashboard">
+      <h1>Vendor Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/create-product">Create New Product</Link>
+        </li>
+        {/* Add other dashboard functionalities here */}
+        <li>View Orders</li>
+        <li>Manage Products</li>
+        <li>Update Profile</li>
+      </ul>
+    </div>
+  );
+};
+
+export default VendorDashboard;
