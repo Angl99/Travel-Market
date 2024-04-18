6f3c86041d4ee2fa2cc499c68233ee16850e20b2
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-04-17 21:44:25 -0400

    updated frontend files for vendor dashboard

--- src/components/VendorView/VendorDashboard.jsx
+++ src/components/VendorView/VendorDashboard.jsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 
 const VendorDashboard = () => {
   return (
+    
     <div className="vendor-dashboard">
-      <h1>Vendor Dashboard</h1>
+        <Link to="/" className="homepage">
+      Home
+    </Link>
+      {/* <h1>Vendor Dashboard</h1> */}
       <ul>
         <li>
           <Link to="/create-product">Create New Product</Link>
